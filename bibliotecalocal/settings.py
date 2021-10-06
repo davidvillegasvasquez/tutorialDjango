b022ef3d6313b4fd64c856677d6a95e2f8810e3a
davidvillegasvásquez <davidvillegas1@hotmail.com>
2021-10-06 11:10:44 -0400

    061121-5

--- bibliotecalocal/settings.py
+++ bibliotecalocal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'serene-wildwood-02865.herokuapp.com']
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1'] se usa si DEBUG = False y no dará ninguna información del error. Sólo una escueta aclaratoria de que el recurso solicitado no se encuentra (404 estandar), en el caso que no le tengamos una nombreapp.urls en la carpeta de la aplicación.
 
 # Application definition
